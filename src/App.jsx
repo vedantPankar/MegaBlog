7926bda3b739e07c292b05e824a92e02e404d4ee
Vedant Pankar <vedantpankar@Vedants-MacBook-Air.local>
2025-10-20 11:59:14 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import "./App.css";
 import authService from "./appwrite/auth";
-import { login, logout } from "./store/authSlice";
+import { login, logout } from "./Store/authSlice";
 import { Footer, Header } from "./components";
 import { Outlet } from "react-router-dom";
 
